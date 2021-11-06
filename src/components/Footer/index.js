a98fa75ca7f2c9321b561ba67db9ec6d06a27828
Topash15 <Alextopash@gmail.com>
2021-11-06 17:15:52 -0400

    added dynamically generated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 
+
 const Footer = () => {
-    return(
-        <div>
-            {/* TODO GitHub, LinkedIn, Resume */}
-            Footer
+    const contacts = [
+        {
+            link: "https://github.com/Topash15",
+            alt: "github",
+            source: 'GitHub32'
+        },
+        {
+            link: "https://www.linkedin.com/in/alexander-topash-667942168/",
+            alt: "linkedin",
+            source: 'LinkedIn-Blue-14@2x'
+        },
+        {
+            link: "https://www.linkedin.com/in/alexander-topash-667942168/",
+            alt: "linkedin",
+            source: 'LinkedIn-Blue-14@2x'
+        },
+    ];
+
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <ul className="">
+              {contacts.map((contact) => (
+                <li className="" key={contact}>
+                  <a href = {contact.link}>
+                    <img
+                        alt={contact.alt}
+                        className = {contact.source}
+                        src= {require(`../../assets/${contact.source}.png`).default}
+                        ></img>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  };
 
 export default Footer
