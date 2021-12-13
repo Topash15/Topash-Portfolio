370934daaa37964f2996edecc70ba7af628b33a7
Topash15 <Alextopash@gmail.com>
2021-12-13 13:37:09 -0500

    adjusted about me

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,36 @@
 import React from "react";
-import "./style.css"
-import Footer from "../Footer"
+import "./style.css";
+import Footer from "../Footer";
 
 const Nav = (props) => {
   const tabs = ["About Me", "My Projects", "Resume", "Contact Me"];
 
   return (
     <nav className="nav-container">
-      <div className = "nav-upper">
-        <h1 className = "myName">Alexander Topash</h1>
-        <h2 className = "myTitle">Full Stack Web Developer</h2>
-        <img className = "headshot" src={require("../../assets/headshot.png").default} alt="headshot"/>
+      {/* <input type="checkbox" />
+      <span></span>
+      <span></span>
+      <span></span> */}
+      
+      <div className="nav-upper">
+        <h1 className="myName">Alexander Topash</h1>
+        <h2 className="myTitle">Full Stack Web Developer</h2>
+        <img
+          className="headshot"
+          src={require("../../assets/headshot.png").default}
+          alt="headshot"
+        />
       </div>
-      <div className = "nav-tabs-container">
-        <ul className = "nav-list">
-          {tabs.map((tab)=>(
-            <li className = "nav-tab" key={tab}>
-              <a href={"#" + tab.toLowerCase()}
-              onClick={() => props.handlePageChange(tab)}
-              className = {props.currentPage === tab ? "nav-link active" : "nav-link"}
+      <div className="nav-tabs-container">
+        <ul className="nav-list">
+          {tabs.map((tab) => (
+            <li className="nav-tab" key={tab}>
+              <a
+                href={"#" + tab.toLowerCase()}
+                onClick={() => props.handlePageChange(tab)}
+                className={
+                  props.currentPage === tab ? "nav-link active" : "nav-link"
+                }
               >
                 {tab}
               </a>
