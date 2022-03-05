354093237ad139814252937dfb166290784d224b
Topash15 <Alextopash@gmail.com>
2022-03-05 14:01:59 -0500

    media queries. fixed mobile view

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -8,12 +8,12 @@ const ContactForm= ()=> {
         {/* <h1 className="contact-title">Contact Me</h1> */}
         <div className='email-div'>
             <h3 className="email-title">Email</h3>
-            <h6 className="email-subtitle"> Preferred method </h6>
+            <p className="email-subtitle"> Preferred method </p>
             <a className="email-link" href="emailto:Alextopash@gmail.com">Alextopash@gmail.com</a>
         </div>
         <div className='phone-div'>
             <h3 className="phone-title">Cell Phone</h3>
-            <h6 className = "phone-subtitle">Call or text</h6>
+            <p className = "phone-subtitle">Call or text</p>
             <a className="phone-number" href="tel:17178779709">(717)-877-9709</a>
         </div>
     </section>)
--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -52,7 +52,7 @@ const Project = () => {
         <div className="card-group">
           <div className="row">
             {projects.map((project) => (
-              <div class="card col-sm-4">
+              <div class="card col-sm-6">
                 <img
                   className="card-img-top card-image"
                   src={
